0067964b27ac9786088f65f89182f7907d4e9cd4
Alexis <alexis.gomel@gmail.com>
2025-05-16 01:56:31 +0200

    added hcanges to deploy online

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from src.layouts.layout import get_layout
 from src.callbacks import register_callbacks, party_set_options, default_start, default_end
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
+server = app.server #expose to flask
 app.layout = get_layout(party_set_options, default_start, default_end)
 register_callbacks(app)
 
