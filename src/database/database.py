cbdb1769c298e87d2332d4d695546da912f1eab9
Alexis <alexis.gomel@gmail.com>
2025-05-16 02:57:07 +0200

    fixed path logic

--- src/database/database.py
+++ src/database/database.py
@@ -3,13 +3,16 @@ import sqlite3
 import datetime
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__name__))
 DB_PATH = r"C:\Users\Alexis\AppData\Local\Mixxx\mixxxdb.sqlite"
 DB_PATH_test= os.path.join(BASE_DIR, 'mixxxdb_subset.sqlite')
 
+if os.path.isfile(DB_PATH):
+    dbpath = DB_PATH
+else: dbpath = DB_PATH_test
+     
 def get_playlists():
-    try: conn = sqlite3.connect(DB_PATH)
-    except: conn = sqlite3.connect(DB_PATH_test)
+    conn = sqlite3.connect(dbpath)
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
     cur.execute("SELECT id, name FROM Playlists")
@@ -38,7 +41,7 @@ def get_playlists():
     return result
 
 def get_tracks_for_playlist(playlist_id):
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(dbpath)
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
     query_with_hidden = """
@@ -68,7 +71,7 @@ def get_tracks_for_playlist(playlist_id):
     return [dict(track) for track in tracks]
 
 def get_crates():
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(dbpath)
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
     cur.execute("SELECT id, name FROM crates")
@@ -77,7 +80,7 @@ def get_crates():
     return [dict(crate) for crate in crates]
 
 def get_songs_not_in_crates():
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(dbpath)
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
     query_with_hidden = """
@@ -101,7 +104,7 @@ def get_songs_not_in_crates():
     return [dict(song) for song in songs]
 
 def get_library_songs():
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(dbpath)
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
     query_with_hidden = "SELECT id, artist, title, album, rating FROM library WHERE hidden = 0"
--- test/test_read.py
+++ test/test_read.py
@@ -1,7 +1,16 @@
 import sqlite3
+import os
 
-db_path = r"C:\DEV\mixx_addon\test\mixxxdb_subset.sqlite"
-conn = sqlite3.connect(db_path)
+BASE_DIR = os.path.dirname(os.path.abspath(__name__))
+DB_PATH = r"C:\Users\Alexis\AppData\Local\Mixxx\mixxxdb.sqlite"
+DB_PATH_test= os.path.join(BASE_DIR, 'mixxxdb_subset.sqlite')
+if os.path.isfile(DB_PATH):
+    dbpath = DB_PATH
+else: dbpath = DB_PATH_test
+
+print(dbpath)
+
+conn = sqlite3.connect(dbpath)
 conn.row_factory = sqlite3.Row
 cur = conn.cursor()
 
